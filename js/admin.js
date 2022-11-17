3f81e154c76fd496b15a60b9db21d6f8053ee5bb
YesIDo49 <mehdialsidcheikh@gmail.com>
2022-11-17 11:14:18 +0100

    :lipstick: updated css and added admin login page

---
+++ js/admin.js
@@ -0,0 +1,28 @@
+function accessAdmin(){
+
+    let login = document.getElementById('login')
+    let password = document.getElementById('password')
+    let redirectAdmin = document.getElementById('redirect-admin')
+    let adminDenied = document.querySelector('.admin__denied')
+
+
+    redirectAdmin.addEventListener('click', (e) => {
+        let valueLogin = login.value;
+        let valuePassword = password.value;
+        console.log(valueLogin)
+        console.log(valuePassword)
+
+        if (valueLogin == "admin" && valuePassword == "mdp"){
+            window.location.href = 'index.html'
+        }else{
+            adminDenied.style.display = "flex"
+            setTimeout(() => {
+                adminDenied.style.display = "none"
+            }, "2000")
+        }
+
+    })
+
+}
+
+accessAdmin()
